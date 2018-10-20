4c84cd57b2d552b3acde8dc72aedf6a6023bcad0
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2018-10-20 16:05:35 +0400

    window

--- src/tracker/core.clj
+++ src/tracker/core.clj
@@ -4,11 +4,6 @@
    [tracker.writer :as twriter]
    [geo.spatial :as spatial]))
 
-(defn distance [x y]
-  (let [xp (-> x :point)
-        yp (-> y :point)]
-    (spatial/distance xp yp)))
-
 (defn speed [x y]
   (let [xp (-> x :point)
         yp (-> y :point)
@@ -19,36 +14,80 @@
         time     (/ (- yt xt) 1000)]
     (/ distance time)))
 
-;; speed-limit м/с
-(defn remove-anomalies [speed-limit]
-  (fn [xf]
-    (let [prev (volatile! ::none)]
-      (fn
-        ([] (xf))
-        ([result] (xf result))
-        ([result input]
-         (if (and (not= ::none @prev)
-                  (< speed-limit (speed @prev input)))
-           result
-           (do
-             (vreset! prev input)
-             (xf result input))))))))
+(defn window [xf]
+  (let [vprev (volatile! nil)]
+    (fn
+      ([] (xf))
+      ([result] (xf result))
+      ([result input]
+       (let [prev @vprev]
+         (vreset! vprev input)
+         (xf result [prev input]))))))
+
+(defn not-anomaly? [speed-limit prev curr]
+  (or
+   (nil? prev)
+   (> speed-limit (speed prev curr))))
 
 
+(defn remove-anomalies [data speed-limit]
+  (into []
+        (comp window
+              (filter (fn [[prev curr]]
+                        (not-anomaly? speed-limit prev curr)))
+              (map last))
+        data))
 
+
+(defn distance [x y]
+  (spatial/distance (:point x) (:point y)))
+
+(defn compress [data n]
+  (let [total-distance (transduce (comp window
+                                        (drop 1)
+                                        (map (fn [[prev curr]]
+                                               (distance prev curr))))
+                                  + 0 filtered)]))
 
 (comment
-  (let [x (treader/load-data "x.json")]
-    (twriter/write "output.json" x)))
-
+  (let [data (treader/load-data "x.json")]
+    (twriter/write "output.json" data)))
 
 (comment
-  (let [x                (treader/load-data "x.json")
+  (let [data             (treader/load-data "x.json")
         speed-limit-km-h 120
         speed-limit      (/ (* speed-limit-km-h 1000) (* 60 60))
-        filtered         (into [] (remove-anomalies speed-limit) x)]
-    (prn (count x)
+        filtered         (remove-anomalies data speed-limit)]
+
+    (prn (count data)
          (count filtered))
     (twriter/write "output.json" filtered)))
 
 
+
+
+        ;; n                50
+        ;; step             (/ total-distance n)
+        ;; anchors          (for [i n] (* i step))
+        ;; reduced          (into []
+        ;;                        (comp
+        ;;                         window
+
+        ;;                         (map last))
+        ;;                        filtered))))
+
+
+
+#_(comment
+    (let [x                (treader/load-data "x.json")
+          speed-limit-km-h 120
+          n                10
+          speed-limit      (/ (* speed-limit-km-h 1000) (* 60 60))
+          compressed       (into []
+                                 (comp
+                                  (remove-anomalies speed-limit)
+                                  (compress 10))
+                                 x)]
+      (prn (count x)
+           (count compressed))
+      (twriter/write "output.json" compressed)))
